2962b3aa6e1a10b517a2b3e329513f26576f1308
DamasoMagno <limamdamaso@gmail.com>
2021-05-14 11:22:19 -0300

    Site quase pronto

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const body = document.querySelector("body");
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+const search = document.querySelector(".search");
 const cityInfo = document.getElementById("city");
 const closeModal = document.querySelector(".close-modal");
 
 let cityWeatherProps;
-let map = L.map('map');
+let leafletMap = L.map('map');
 let cityProps;
+let cities = [];
+
+
+function renderImage(imageURL){
+  return `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(${imageURL})`;
+}
 
 const SearchCity = {
   async showCity(){
@@ -14,22 +21,23 @@ const SearchCity = {
       return;
     };
 
-    await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=d13978d5&city_name=${input.value}`)
-    .then(response => response.json())
-    .then(response => {
-      if(response.results.city_name === input.value){
-        cityWeatherProps = response.results
+    await axios.get(`https://api.hgbrasil.com/weather?format=json-cors&key=548bd378&city_name=${input.value}&locale=pt`)
+    .then( response => {
+      const { data } = response;
+      if(data.results.city_name === input.value){
+        cityWeatherProps = data.results
         input.style.border = "1px solid rgba(0,0,0,0.1)";
       } else {
         alert("Cidade não cadastrada no sistema");
       }
     });
 
-    await fetch(`https://city-weather-api.herokuapp.com/place-details?input=${cityWeatherProps.city}`)
-    .then(response => response.json())
-    .then(response => cityProps = response );
+    await axios.get(`https://city-weather-api.herokuapp.com/place-details?input=${cityWeatherProps.city}`)
+    .then(response => {
+      cityProps = response.data
+    });
 
-    cityInfo.style.background = `url(${cityProps.imageURL})`;
+    cityInfo.style.background = renderImage(cityProps.imageURL);
 
     DOM.updateDegress();
     Modal.showMap();
@@ -37,14 +45,13 @@ const SearchCity = {
 }
 
 const DOM = {
-  degress: document.querySelector("#degress"),
-  region: document.querySelector("#region"), 
-  image: document.querySelector("#dayOrNight"),
+  degress: document.querySelector(".degress"),
+  region: document.querySelector(".region"), 
+  image: document.querySelector(".dayOrNight"),
 
   updateDegress(){
     DOM.degress.innerHTML = `${cityWeatherProps.temp}º`;
     DOM.region.innerHTML = `${cityWeatherProps.city}`;
-
     DOM.image.src = `assets/${cityWeatherProps.currently}.png`;
   }
 }
@@ -58,26 +65,24 @@ const Modal = {
 
   reloadMyMap(){
     let mymap = cityProps ?
-    map.setView([cityProps.coordinates.lat, cityProps.coordinates.lng], 15) : 
-    map.setView([-23.1697205, -47.1473495], 11);
+    leafletMap.setView([cityProps.coordinates.lat, cityProps.coordinates.lng], 15) : 
+    leafletMap.setView([-23.1697205, -47.1473495], 11);
 
     return mymap;
   },
 
   showMap(){
-    let mymap = Modal.reloadMyMap();
-
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiZGFtYXNvbWFnbm8iLCJhIjoiY2tnNnh4OHIzMDIwYjJwbndjZXJwdzJlYiJ9.kVuWDJS8DvJgmtDyqr1ujQ'
-    }).addTo(mymap);
+    }).addTo(Modal.reloadMyMap());
   },
 }
 
 Modal.showMap();
 
-button.addEventListener("click", SearchCity.showCity);
+search.addEventListener("click", SearchCity.showCity);
 cityInfo.addEventListener("click", Modal.toggleModal);
 closeModal.addEventListener("click", Modal.toggleModal);
